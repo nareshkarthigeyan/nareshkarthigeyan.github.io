c636bfcadc7492e6e3985b1b48d74634f95b5038
Naresh Karthigeyan <nareshkarthigeyan.2005@gmail.com>
2024-10-14 21:37:30 +0530

    svelte migrate

---
+++ src/routes/+page.svelte
@@ -0,0 +1,20 @@
+<script>
+    import Name from "./Name.svelte";
+</script>
+
+<div class="main">
+    <div class="title">
+        <Name />
+    </div>
+    
+</div>
+
+<style>
+.main {
+    display: flex;
+}
+.title {
+    padding-left: 20px;
+    margin-top: 10px;
+}
+</style>
---
+++ src/routes/Name.svelte
@@ -0,0 +1,31 @@
+<script>
+    const name = "nareshkarthigeyan"
+    let caption = "official personal web";
+</script>
+
+<div class="title">
+    {name}
+</div>
+<div class="balls">
+    {caption}
+</div>
+
+<style>
+    .title, .balls {
+        font-family: 'Times New Roman', Times, serif;
+        font-style: bold;
+        font-style: italic;
+        color: #A8FF07;
+        margin: 0px;
+    }
+    
+    .title {
+        font-size: 100px;
+        margin-bottom: -15px;
+    }
+
+    .balls {
+        font-size: 30px;
+    }
+
+</style>
