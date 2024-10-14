43a9b0697fe5b26310cbf13698c6a4238a7022ef
Naresh Karthigeyan <nareshkarthigeyan.2005@gmail.com>
2024-10-14 22:41:37 +0530

    photo

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,12 +1,15 @@
 <script>
     import Name from "./Name.svelte";
+  import Photo from "./Photo.svelte";
 </script>
 
 <div class="main">
     <div class="title">
         <Name />
     </div>
-    
+</div>
+<div class="secondhalf">
+   <Photo />
 </div>
 
 <style>
@@ -16,5 +19,10 @@
 .title {
     padding-left: 20px;
     margin-top: 10px;
+}
+
+.secondhalf {
+    padding-left: 20px;
+    padding-top: 20px;
 }
 </style>
---
+++ src/routes/Photo.svelte
@@ -0,0 +1,11 @@
+<script>
+    let alt = 'my image'
+</script>
+
+<img src="/naresh.png" alt={alt} />
+
+<style>
+    img {
+        height: 600px;
+    }
+</style>
